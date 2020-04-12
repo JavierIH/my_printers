631801489a8e52f0fb8140050f65b92d973906fb
JavierIH <javier.isabel.hernandez@gmail.com>
2020-04-13 00:20:06 +0200

    updated machine dimensions

--- MendelMaxXl/firmware/Marlin/Configuration.h
+++ MendelMaxXl/firmware/Marlin/Configuration.h
@@ -1092,8 +1092,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 420
-#define Y_BED_SIZE 250
+#define X_BED_SIZE 410
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1101,7 +1101,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 280
+#define Z_MAX_POS 220
 
 /**
  * Software Endstops
